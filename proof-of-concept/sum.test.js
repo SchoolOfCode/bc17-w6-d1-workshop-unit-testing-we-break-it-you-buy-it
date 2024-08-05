cbe006a5f07d07dbbbdf8c99826b8dff472761cc
dom-m17 <dominicmaynard00@gmail.com>
2024-08-05 11:44:15 +0100

    ticket 1 and 2

---
+++ proof-of-concept/sum.test.js
@@ -0,0 +1,11 @@
+//  import `test` and `expect` functions from Vitest
+//  import the `sum` function from sum.js
+//  create a bare bones test
+//  run Vitest to check that it detects and passes the new test
+//  inside the test:
+//      call the sum function with arguments: 5 and 7
+//      assert that the value returned is 12
+
+import { test, expect } from 'vitest';
+import { sum } from './sum.js'
+
