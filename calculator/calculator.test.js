d1184bb8a18d8e67e54dd17ee0cbd23df62d1ae0
dom-m17 <dominicmaynard00@gmail.com>
2024-08-05 13:45:12 +0100

    mulitply and divide tests

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -3,16 +3,26 @@ import { test, expect } from "vitest";
 import { calculate } from "./calculator.js";
 
 // test addition
-test("adds 9 + 10 to equal 19", () => {
+test("adds 9 and 10 to equal 19", () => {
     expect(calculate("+", 9, 10)).toBe(19);
 });
 
 // test subtraction
-test("adds 10 - 9 to equal 1", () => {
+test("subtracts 9 from 10 to equal 1", () => {
     expect(calculate("-", 10, 9)).toBe(1);
 });
 
 // test multiplication
+test("multiplies 3 by 8 to equal 24", () => {
+    expect(calculate("*", 3, 8)).toBe(24);
+});
+
+
 // test division
+test("divides 12 by 2 to equal 6", () => {
+    expect(calculate("/", 12, 2)).toBe(6);
+});
+
+
 // test squaring
 // test throwing an error for an unsupported operator
